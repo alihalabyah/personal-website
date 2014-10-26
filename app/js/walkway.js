ab5b8548a0c0e7df00a61293274ae43c98fa6472
Connor Atherton <connor@bitnami.com>
2014-10-26 00:12:42 -0700

    Change to jquery onload

--- app/js/walkway.js
+++ app/js/walkway.js
@@ -1,14 +1,9 @@
-function animate() {
+$(function animate() {
   var svg = new Walkway({
     selector: '#example',
     easing: 'easeInOutCubic',
     duration: 2100
   });
   svg.draw();
-}
+});
 
-if (document.addEventListener) {
-  document.addEventListener("DOMContentLoaded", animate, false);
-} else {
-  animate();
-}
